012ee9c9d87fb646f7193956eb5c081b79c7d4dc
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-04-03 15:52:46 -0600

    Added more tests

--- code.test.js
+++ code.test.js
@@ -2,5 +2,24 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
+function factorial(n) {
+    if(n === 0) return 1;
+    else return n * factorial(n - 1);
+}
+
+function e_test(n) {
+    if(n === 0) return 1;
+    else return 1.0 / factorial(n) + e(n - 1);
+}
+
+// Original tests
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
+
+// Added tests
+const test =
+    jsc.forall("nat", function(n) {
+        return JSON.stringify(e_test(n)) ==
+            JSON.stringify(e(n));
+    });
+jsc.assert(test);
